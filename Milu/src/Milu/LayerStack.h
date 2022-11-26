030c63e935f115f1f4938a15d9c0a6d95737ebe8
OSevenCheng <osevencheng@163.com>
2022-11-26 18:51:20 +0800

    Add layer

---
+++ Milu/src/Milu/LayerStack.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "mlpch.h"
+
+#include "Milu/Core.h"
+#include "Layer.h"
+
+namespace Milu
+{
+    class MILU_API LayerStack final
+    {
+    public:
+        LayerStack();
+        ~LayerStack();
+
+        void PushLayer(Layer* _pLayer);     //insert a layer at m_LayerInsert, and m_LayerInsert++
+        void PushOverlay(Layer* _pOverlay); //pushback a layer without changing m_LayerInsert
+        void PopLayer(Layer* _pLayer);      //remove a layer and m_LayerInsert--
+        void PopOverlay(Layer* _pOverlay);  //find a layer from m_LayerInsert to the end, and remove it
+
+        std::vector<Layer*>::iterator begin() {
+            return m_Layers.begin();
+        }
+        std::vector<Layer*>::iterator end() {
+            return m_Layers.end();
+        }
+    private:
+        std::vector<Layer*> m_Layers;
+        std::vector<Layer*>::iterator m_LayerInsert;
+    };
+};
+
